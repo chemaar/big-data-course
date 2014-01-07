8ea8ddd24363923eb36fae9e05d01387321c7b62
Jose María Alvarez <chema.ar@gmail.com>
2014-01-07 23:02:10 +0100

    Adding final examples

--- prototypes/relatweet/src/main/java/es/josemalvarez/relatweet/naive/TweetStreamMain.java
+++ prototypes/relatweet/src/main/java/es/josemalvarez/relatweet/naive/TweetStreamMain.java
@@ -1,6 +1,7 @@
 package es.josemalvarez.relatweet.naive;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import twitter4j.StallWarning;
 import twitter4j.Status;
@@ -18,21 +19,23 @@ public class TweetStreamMain {
 	
 		 public static void main(String[] args) throws TwitterException {
 			 //Remember to configure twitter4j.properties
+			 	final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss");
 		        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 		        StatusListener listener = new StatusListener() {
 		 
 					public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-						 System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+						//System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
 						
 					}
 
 					public void onStallWarning(StallWarning warning) {
-						  System.out.println("Got stall warning:" + warning);
+						//System.out.println("Got stall warning:" + warning);
 						
 					}
 
 					public void onStatus(Status status) {
-						 System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+					//	 System.out.println("@" + status.getUser().getScreenName()+","+sdf.format(status.getCreatedAt()) + "," + status.getText());
+						 System.out.println(status.getUser().getScreenName()+","+sdf.format(status.getCreatedAt()) + "," + status.getText());
 						
 					}
 
@@ -42,12 +45,12 @@ public class TweetStreamMain {
 					}
 
 					public void onScrubGeo(long userId, long upToStatusId) {
-						  System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+						  //System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
 						
 					}
 
 					public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-						System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+						//System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
 						
 					}
 		        };
