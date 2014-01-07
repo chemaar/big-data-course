24c934238dab0f888728e5834e76384e078d0d75
Jose María Alvarez <chema.ar@gmail.com>
2014-01-07 22:16:32 +0100

    Adding new examples

---
+++ prototypes/mapreduce-intro/src/main/java/es/josemalvarez/mapreduce/examples/AverageDriver.java
@@ -0,0 +1,168 @@
+package es.josemalvarez.mapreduce.examples;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+
+
+/**
+ * Examples adapted from the book Map/Reduce patterns http://shop.oreilly.com/product/0636920025122.do
+ * @author chema
+ *
+ * Given a list of tweets in the form: (username, date, text)
+ * 
+ * determine the average comment length per hour of day.
+ *
+ *
+ *
+ */
+
+public class AverageDriver {
+
+	public static class SOAverageMapper extends
+			Mapper<Object, Text, IntWritable, CountAverageTuple> {
+
+		private IntWritable outHour = new IntWritable();
+		private CountAverageTuple outCountAverage = new CountAverageTuple();
+
+
+		@SuppressWarnings("deprecation")
+		@Override
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+
+			// Parse the input string into a nice map
+			Map<String, String> parsed = MRDPUtils.parse(value.toString());
+
+			// Grab the "CreationDate" field,
+			// since it is what we are grouping by
+			
+			String strDate = parsed.get(MRDPUtils.CREATION_DATE);
+
+			// Grab the comment to find the length
+			String text = parsed.get(MRDPUtils.TEXT);
+
+			// .get will return null if the key is not there
+			if (strDate == null || text == null) {
+				// skip this record
+				return;
+			}
+
+			try {
+				// get the hour this comment was posted in
+				Date creationDate = MRDPUtils.frmt.parse(strDate);
+				outHour.set(creationDate.getHours());
+
+				// get the comment length
+				outCountAverage.setCount(1);
+				outCountAverage.setAverage(text.length());
+
+				// write out the user ID with min max dates and count
+				context.write(outHour, outCountAverage);
+
+			} catch (ParseException e) {
+				System.err.println(e.getMessage());
+				return;
+			}
+		}
+	}
+
+	public static class SOAverageReducer
+			extends
+			Reducer<IntWritable, CountAverageTuple, IntWritable, CountAverageTuple> {
+		private CountAverageTuple result = new CountAverageTuple();
+
+		@Override
+		public void reduce(IntWritable key, Iterable<CountAverageTuple> values,
+				Context context) throws IOException, InterruptedException {
+
+			float sum = 0;
+			float count = 0;
+
+			// Iterate through all input values for this key
+			for (CountAverageTuple val : values) {
+				sum += val.getCount() * val.getAverage();
+				count += val.getCount();
+			}
+
+			result.setCount(count);
+			result.setAverage(sum / count);
+
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: AverageDriver <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "StackOverflow Average Comment Length");
+		job.setJarByClass(AverageDriver.class);
+		job.setMapperClass(SOAverageMapper.class);
+		job.setCombinerClass(SOAverageReducer.class);
+		job.setReducerClass(SOAverageReducer.class);
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(CountAverageTuple.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
+	public static class CountAverageTuple implements Writable {
+		private float count = 0f;
+		private float average = 0f;
+
+		public float getCount() {
+			return count;
+		}
+
+		public void setCount(float count) {
+			this.count = count;
+		}
+
+		public float getAverage() {
+			return average;
+		}
+
+		public void setAverage(float average) {
+			this.average = average;
+		}
+
+
+		public void readFields(DataInput in) throws IOException {
+			count = in.readFloat();
+			average = in.readFloat();
+		}
+
+
+		public void write(DataOutput out) throws IOException {
+			out.writeFloat(count);
+			out.writeFloat(average);
+		}
+
+		@Override
+		public String toString() {
+			return count + "\t" + average;
+		}
+	}
+}
---
+++ prototypes/mapreduce-intro/src/main/java/es/josemalvarez/mapreduce/examples/MRDPUtils.java
@@ -0,0 +1,32 @@
+package es.josemalvarez.mapreduce.examples;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.apache.commons.lang.StringUtils;
+
+public class MRDPUtils {
+
+	public static final String CREATION_DATE = "CreationDate";
+	public static final String USER_ID = "UserId";
+	public static final String TEXT = "Text";
+	public final static SimpleDateFormat frmt = new SimpleDateFormat(
+			"yyyy-MM-dd' 'HH:mm:ss");
+	
+	public static Map<String, String> parse(String string) {
+		String [] values = string.split(",");
+		Map<String, String> fields = new HashMap<String,String>(3);
+		System.out.println(string);
+		fields.put(USER_ID, values[0]);
+		fields.put(CREATION_DATE, values[1]);
+		String text = "";
+		for(int i = 2; i<values.length;i++){
+			text += values[i];
+		}
+		fields.put(TEXT, text);
+		return fields;
+	}
+
+}
